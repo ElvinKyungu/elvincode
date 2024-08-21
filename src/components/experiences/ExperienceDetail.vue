8e79fb5e92d3c7626dae6df3ff01c241b0f49fb2
elvinkyungu <elvinkyungu.75@gmail.com>
2024-08-21 21:22:38 +0200

    feat: add ExperienceDetail type and integrate experience data into the template

---
+++ src/components/experiences/ExperienceDetail.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { ExperienceDetail } from '@/types/experiences.ts'
+import SkillsContent from './SkillsContent.vue'
+
+defineProps<({
+  experiences: ExperienceDetail
+})>()
+</script>
+<template>
+  <div ref="rightPanel">
+    <div class="flex justify-between">
+      <h1 class="text-xl">{{ experiences.companyName }}</h1>
+      <IconDots class="cursor-pointer"/>
+    </div>
+    <div class="relative">
+      <div class="border-b mt-7 border-green-400">
+        <h2 class="text-3xl pb-5">{{ experiences.duration }}</h2>
+      </div>
+      <div class="flex w-full">
+        <div class=" w-1/2 flex items-end gap-3">
+          <p class="border border-gray-600 rounded-full py-1 px-4 text-sm">⏱️ {{ experiences.workType.time }}</p>
+          <p class="border border-gray-600 rounded-full py-1 px-4 text-sm">📍 {{ experiences.workType.location }}</p>
+        </div>
+        <div class=" w-1/2 flex justify-end items-end gap-4 -mt-20">
+          <div class="rounded-full h-20 z-50 w-2 bg-yellow-200"></div>
+          <div class="rounded-full h-14 z-50 w-2 bg-yellow-100"></div>
+          <div class="rounded-full h-28 z-50 w-2 bg-yellow-300"></div>
+          <div class="rounded-full h-40 z-50 w-2 bg-green-400"></div>
+          <div class="rounded-full h-28 z-50 w-2 bg-white"></div>
+          <div class="rounded-full h-32 z-50 w-2 bg-yellow-500"></div>
+          <div class="rounded-full h-24 z-50 w-2 bg-green-200"></div>
+        </div>
+      </div>
+    </div>
+    <h1 class="mt-7 text-white text-2xl">What I learned from this work</h1>
+    <div class="flex justify-start gap-5 mt-5">
+      <button class="bg-white text-black py-2 px-5 rounded-full">Soft Skills</button>
+      <button class="bg-white/10 py-2 px-5 rounded-full">Hard Skills</button>
+      <button class="bg-white/10 py-2 px-5 rounded-full">Creative Skills</button>
+    </div>
+    <SkillsContent :skills="experiences.softSkills" />
+  </div>
+</template>
--- src/views/Experiences.vue
+++ src/views/Experiences.vue
@@ -3,14 +3,45 @@ import { useDark } from "@vueuse/core"
 import Experience from "@/components/experiences/Experiences.vue"
 import Header from "@/components/base/Header.vue"
 import IconChek from "@/components/icons/IconChek.vue"
-import IconDots from '@/components/icons/IconDots.vue'
-import IconTime from '@/components/icons/IconTime.vue'
-import IconAnalitical from '@/components/icons/IconAnalitical.vue'
-import IconFlag from '@/components/icons/IconFlag.vue'
 import experiences from '@/data/experiences.json'
+import ExperiencesDetails from '@/components/experiences/ExperienceDetail.vue'
+import type { ExperienceDetail } from '@/types/experiences'
 
 const isDark = useDark()
-
+const efficientOutsourceExperience: ExperienceDetail = {
+  companyName: "Efficient Outsource SRL",
+  duration: "5 months of code",
+  workType: {
+    location: 'Full Remote',
+    time: 'Full Time',
+  },
+  softSkills: [
+    {
+      title: "Analytical skills",
+      description: "Each time I had to find solutions to new challenges, which pushed me to give my all, analyze the problem and live up to the company's expectations.",
+      image: "https://media.licdn.com/dms/image/v2/D4D03AQGcMcti8SAadg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1691835487556?e=1729728000&v=beta&t=9TlfE178JTSbViigb1IFca_ILLxVc5S3Q-QNzP4J5nI",
+      isHigh: false,  // Middle skill level
+    },
+    {
+      title: "Time management",
+      description: "I learned to manage my time and tasks very well, which helped me avoid technical debts.",
+      image: "https://media.licdn.com/dms/image/v2/D4D03AQGcMcti8SAadg/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1691835487556?e=1729728000&v=beta&t=9TlfE178JTSbViigb1IFca_ILLxVc5S3Q-QNzP4J5nI",
+      isHigh: true,  // High skill level
+    },
+    {
+      title: "Communication",
+      description: "I communicated very well with the team.",
+      image: "https://infobiz.md/storage/companies/img/1024600042553.png",
+      isHigh: true,  
+    }
+  ],
+  hardSkills: [
+   
+  ],
+  creativeSkills: [
+    
+  ],
+};
 </script>
 <template>
   <main class="min-h-screen w-full" :class="{ 'text-white bg-[#222]': isDark }">
@@ -37,7 +68,7 @@ const isDark = useDark()
               </Experience>
             </div>
             <div class="bg-black/10 rounded-lg py-5 px-10 relative hidden md:block">
-              
+              <ExperiencesDetails :experiences="efficientOutsourceExperience"/>
             </div>
           </div>
         </div>
